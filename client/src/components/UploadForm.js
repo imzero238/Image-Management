06f435a1714b52039992907172d1c996a053ed67
nayoung <evelyn82ny@gmail.com>
2022-05-28 20:53:56 +0900

    Feat: Send the image to the backend (use axios)

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const UploadForm = () => {
     const [file, setFile] = useState(null);
     const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요");
+
+    const imageSelectHandler = (event) => {
+        const imageFile = event.target.files[0];
+        setFile(imageFile);
+        setFileName(imageFile.name);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("image", file);
+        try {
+            const res = await axios.post("/upload", formData, {
+                headers: {"Content-Type": "multipart/form-data"}
+            });
+            console.log({res});
+            alert("success!!");
+        } catch (err) {
+            alert("fail!!");
+            console.error(err);
+        }
+    };
+
     return  (
-        <form>
+        <form onSubmit={onSubmit}>
             <label htmlFor="image">{fileName}</label>
             <input 
                 id="image" type="file" 
-                onChange={(event) => {
-                    const imageFile = event.target.files[0];
-                    setFile(imageFile);
-                    setFileName(imageFile.name);
-                }}
+                onChange = {imageSelectHandler}
             />
             <button type="submit">제출</button>
         </form>
