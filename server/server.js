a824aea226fa7934e551022048adb331a8ecd1b1
nayoung <evelyn82ny@gmail.com>
2022-05-28 16:09:40 +0900

    Feat: Upload image(Backend: Node.js)

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+});
+const upload = multer({storage, 
+    fileFilter: (req, file, cb) => {
+        if(["image/jpeg", "image/png"].includes(file.mimetype)) cb(null, true);
+        else cb(new Error("invalid file type"), false);
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    }
+});
 
 const app = express();
 const PORT = 5050;
 
-app.post('/upload', (req, res) => {
-    console.log('/upload called!');
-    res.json({result: "success"});
+app.use("/uploads", express.static("uploads"));
+
+app.post('/upload', upload.single("image"), (req, res) => {
+    console.log(req.file);
+    res.json(req.file);
 });
 
 app.listen(PORT, () => console.log("Express server listening on PORT" + PORT));
