52eff82486aff634ddc7b2f6c78357bbce7ba408
nayoung <evelyn82ny@gmail.com>
2022-05-29 14:01:19 +0900

    Build: Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, "./uploads"),
     filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
 });
-const upload = multer({storage, 
+const upload = multer({
+    storage, 
     fileFilter: (req, file, cb) => {
         if(["image/jpeg", "image/png"].includes(file.mimetype)) cb(null, true);
         else cb(new Error("invalid file type"), false);
@@ -19,12 +22,15 @@ const upload = multer({storage,
 
 const app = express();
 const PORT = 5050;
+console.log(process.env);
 
-app.use("/uploads", express.static("uploads"));
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected.")
 
-app.post('/upload', upload.single("image"), (req, res) => {
-    console.log(req.file);
-    res.json(req.file);
-});
-
-app.listen(PORT, () => console.log("Express server listening on PORT" + PORT));
+    app.use("/uploads", express.static("uploads"));
+    app.post('/upload', upload.single("image"), (req, res) => { res.json(req.file);});
+    app.listen(PORT, () => console.log("Express server listening on PORT" + PORT));
+})
+.catch((err) => console.log(err));
