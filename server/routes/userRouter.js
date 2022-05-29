38af31523c79d7eac56c02a652afb93667e0010f
nayoung <evelyn82ny@gmail.com>
2022-05-30 02:21:44 +0900

    Feat: Add conditions for sign-up

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,13 +4,22 @@ const User = require("../models/User");
 const { hash } = require("bcryptjs");
 
 userRouter.post("/register", async(req, res) => {
-    const hashedPassword = await hash(req.body.password, 10);
-    await new User({
+    try {
+        if(req.body.password.length < 6) 
+            throw new Error("password는 6자 이상으로 입력해주세요.");
+        if(req.body.username.length < 3)
+            throw new Error("username은 3자 이상으로 입력해주세요.");
+        
+        const hashedPassword = await hash(req.body.password, 10);
+        await new User({
         name: req.body.name,
         username: req.body.username,
         hashedPassword
-    }).save();
-    res.json({ message: "user registered" });
+        }).save();
+        res.json({ message: "user registered" });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 module.exports = { userRouter };
