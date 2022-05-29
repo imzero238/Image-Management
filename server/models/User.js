8d3912dbae530a2bb1ad215ef479a5e9940d7c64
nayoung <evelyn82ny@gmail.com>
2022-05-30 02:12:36 +0900

    Feat: Generate a hashed password

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
     {
         name: { type: String, required: true},
         username: { type: String, required: true, unique: true },
-        password: {type: String, required: true}
+        hashedPassword: {type: String, required: true}
     },
     { timestamps: true }
 );
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
+const { hash } = require("bcryptjs");
 
 userRouter.post("/register", async(req, res) => {
-    const user = await new User(req.body).save();
+    const hashedPassword = await hash(req.body.password, 10);
+    await new User({
+        name: req.body.name,
+        username: req.body.username,
+        hashedPassword
+    }).save();
     res.json({ message: "user registered" });
 });
 
