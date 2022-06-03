e9572894c7bd75aebfd4723b808268a83ddf2c9c
nayoung <evelyn82ny@gmail.com>
2022-06-03 12:15:11 +0900

    Feat: Delete image

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -2,6 +2,11 @@ const { Router } = require("express");
 const imageRouter = Router();
 const Image = require("../models/Image");
 const { upload } = require("../middleware/imageUpload");
+const fs = require("fs");
+const { promisify } = require("util");
+const mongoose = require("mongoose");
+
+const fileUnlink = promisify(fs.unlink);
 
 imageRouter.post("/", upload.single("image"), async (req, res) => { 
     try{
@@ -28,6 +33,24 @@ imageRouter.post("/", upload.single("image"), async (req, res) => {
 imageRouter.get("/", async (req, res) => {
     const images = await Image.find({ public: true });
     res.json(images);
+});
+
+imageRouter.delete("/:imageId", async (req, res) => {
+    try{
+        if(!req.user)
+            throw new Error("권한이 없습니다.");
+        if(!mongoose.isValidObjectId(req.params.imageId))
+            throw new Error("올바르지 않는 이미지입니다.");
+
+        const image = await Image.findOneAndDelete({ _id: req.params.imageId });
+        if(!image)
+            return res.json({ message: "요청하신 이미지는 이미 삭제되었습니다."});
+        await fileUnlink(`./uploads/${image.key}`);
+        res.json({ message: "이미지가 삭제되었습니다. "});
+    } catch(err){
+        console.log(err);
+        res.status(400).json({ message: err.message });
+    }
 });
 
 module.exports = { imageRouter };
