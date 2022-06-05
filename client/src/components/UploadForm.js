be71347051bc19f177e66041657d845e7c0864a9
nayoung <evelyn82ny@gmail.com>
2022-06-05 16:39:10 +0900

    Feat: Change the location of the uploaded image to the top

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -6,7 +6,7 @@ import ProgressBar from "./ProgressBar";
 import { ImageContext } from "../context/ImageContext";
 
 const UploadForm = () => {
-    const {setImages} = useContext(ImageContext);
+    const {setImages, setPrivateImages} = useContext(ImageContext);
     const [files, setFiles] = useState(null);
     const [previews, setPreviews] = useState([]);
     const [percent, setPercent] = useState(0);
@@ -45,7 +45,8 @@ const UploadForm = () => {
                     setPercent(Math.round((100 * e.loaded) / e.total));
                 },
             });
-            setImages((prevData) => [...prevData, ...res.data]);
+            if(isPublic) setImages((prevData) => [...res.data, ...prevData]);
+            setPrivateImages((prevData) => [...res.data, ...prevData])
             toast.success("이미지 업로드 성공!");
             setTimeout(() => {
                 setPercent(0);
