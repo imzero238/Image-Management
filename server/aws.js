e9dc2ed60aad602a001860195f28f686ab341d38
nayoung <evelyn82ny@gmail.com>
2022-06-08 02:22:09 +0900

    Create a pre-signed URL API

--- server/aws.js
+++ server/aws.js
@@ -7,4 +7,26 @@ const s3 = new aws.S3({
   region: "ap-northeast-2",
 });
 
-module.exports = { s3 };
+const getSignedUrl = ({ key }) => {
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost(
+      {
+        Bucket: "image-upload-management",
+        Fields:  {
+          key,
+        },
+        Expires: 300,
+        Conditions: [
+          ["content-length-range", 0, 50 * 1000 * 1000],
+          ["starts-with", '$Content-Type', "image/"],
+        ],
+      },
+      (err, data) => {
+        if(err) reject(err);
+        resolve(data);
+      }
+    );
+  });
+};
+
+module.exports = { s3, getSignedUrl };
--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -5,9 +5,33 @@ const { upload } = require("../middleware/imageUpload");
 const fs = require("fs");
 const { promisify } = require("util");
 const mongoose = require("mongoose");
-const { s3 } = require("../aws");
+const { s3, getSignedUrl } = require("../aws");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
 
-const fileUnlink = promisify(fs.unlink);
+imageRouter.post("/presigned", async (req, res) => {
+    try{
+        if(!req.user)
+            throw new Error("권한이 없습니다.");
+
+        const { contentTypes } = req.body;
+        if(!Array.isArray(contentTypes))
+            throw new Error("Invalid contentTypes");
+        
+        const presignedData = await Promise.all(
+            contentTypes.map(async (contentType) => {
+                const imageKey = `${uuid()}.${mime.extension(contentType)}`;
+                const key = `raw/${imageKey}`;
+                const presigned = await getSignedUrl({ key });
+                return { imageKey, presigned };
+            })
+        );
+        res.json(presignedData);
+    } catch(err){
+        console.log(err);
+        res.status(400).json({ message: err.message });
+    }
+});
 
 imageRouter.post("/", upload.array("image", 5), async (req, res) => { 
     try{
