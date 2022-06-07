16ed801809f67e7eeabecf4785f218cc1e039214
nayoung <evelyn82ny@gmail.com>
2022-06-07 23:25:14 +0900

    Amazon S3 connection

---
+++ server/aws.js
@@ -0,0 +1,10 @@
+const aws = require("aws-sdk");
+const { AWS_SECRET_KEY, AWS_ACCESS_KEY } = process.env;
+
+const s3 = new aws.S3({
+  secretAccessKey: AWS_SECRET_KEY,
+  accessKeyId: AWS_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
+module.exports = { s3 };
